14922118c1bf36c06e97689e38c43b03dc95e65e
evrwaeq <evrwaeq@gmail.com>
2025-08-26 18:33:41 -0300

    feat: add base UseCase application and tests

---
+++ application/src/main/java/com/codeflix/admin/video/catalog/application/UseCase.java
@@ -0,0 +1,10 @@
+package com.codeflix.admin.video.catalog.application;
+
+import com.codeflix.admin.video.catalog.domain.Category;
+
+public class UseCase {
+
+  public Category execute() {
+    return new Category();
+  }
+}
